43b48c7cee650f6e8a921330c16a7d4d7345715a
anishgc314 <anishgc314@gmail.com>
2025-08-06 22:16:34 +0530

    Add files via upload

---
+++ 01_variable.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+  // int length=3;
+  // int breadth=4;
+int length,breadth;
+  printf("enter the length:\n");
+  scanf("%d",&length);
+
+  printf("enter the breadth:\n");
+  scanf("%d",&breadth);
+  
+   printf("the area of rectangle is %d",length*breadth);
+    return 0;
+    
+}
